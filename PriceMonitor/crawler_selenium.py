44298036d1158c2acda8f28cb54a103b52d9795a
YangZhendong <404013419@qq.com>
2019-02-15 12:05:19 +0800

    Bug修复：修复selenium无法爬取京东商品bug

--- PriceMonitor/crawler_selenium.py
+++ PriceMonitor/crawler_selenium.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # coding=utf-8
+import re
+
 from gevent import monkey  # IMPORT: must import gevent at first
 monkey.patch_all()
 import logging
@@ -32,22 +34,63 @@ class Crawler(object):
         self.chrome.set_script_timeout(60)
 
     def get_jd_item(self, item_id):
-        item_info = ()
-        # url = 'http://httpbin.org/ip'
+        item_info = []
         url = 'https://item.jd.com/' + item_id + '.html'
         try:
             self.chrome.get(url)
-            # print('source', self.chrome.page_source)
-            name = self.chrome.find_element_by_xpath("//*[@class='sku-name']").text
-            price = self.chrome.find_element_by_xpath("//*[@class='p-price']").text
-            subtitle = self.chrome.find_element_by_xpath("//*[@id='p-ad']").text
-            plus_price = self.chrome.find_element_by_xpath("//*[@class='p-price-plus']").text  # may get null
-            item_info = (name, price[1:], subtitle, plus_price[1:])
-            logging.info('Crawl SUCCESS: %s', item_info)
-        except NoSuchElementException as e:
-            logging.warning('Crawl failure: %s', e)
         except TimeoutException as e:
-            logging.warning('Crawl failure: %s', e)
+            logging.warning('Crawl failure: {}'.format(e))
+            return item_info
+
+        # 提取商品名称
+        try:
+            name = self.chrome.find_element_by_xpath("//*[@class='sku-name']").text
+            item_info.append(name)
+        except NoSuchElementException:
+            try:
+                # 加油卡200117841739需要改为提取name
+                name = self.chrome.find_element_by_xpath("//*[@class='name']").text
+                item_info.append(name)
+            except NoSuchElementException as e:
+                item_info.append(None)
+                logging.warning('Crawl name failure: {}'.format(e))
+
+        # 提取商品PLUS价格
+        try:  # 海外购(32533649560)页面无p-price-plus元素，直接None
+            plus_price = self.chrome.find_element_by_xpath("//*[@class='p-price-plus']").text
+            if plus_price:
+                plus_price_xpath = re.findall(r'-?\d+\.?\d*e?-?\d*?', plus_price)
+                item_info.append(plus_price_xpath[0])  # 提取浮点数
+        except NoSuchElementException as e:
+            item_info.append(None)
+            logging.warning('Crawl plus_price failure: {}'.format(e))
+
+        # 提取商品副标题
+        try:
+            subtitle = self.chrome.find_element_by_xpath("//*[@id='p-ad']").text
+            item_info.append(subtitle)
+        except NoSuchElementException:
+            try:
+                # 加油卡200117841739需要改为提取name-s
+                subtitle = self.chrome.find_element_by_xpath("//*[@class='name-s']").text
+                item_info.append(subtitle)
+            except NoSuchElementException as e:
+                item_info.append(None)
+                logging.warning('Crawl subtitle failure: {}'.format(e))
+
+        # 提取商品价格
+        try:
+            price = self.chrome.find_element_by_xpath("//*[@class='p-price']").text
+            if price:
+                price_xpath = re.findall(r'-?\d+\.?\d*e?-?\d*?', price)
+                if price_xpath:  # 若能提取到值
+                    item_info.append(price_xpath[0])  # 提取浮点数
+        except NoSuchElementException as e:
+            logging.warning('Crawl failure: {}'.format(e))
+            item_info.append(None)
+            return item_info
+
+        logging.info('Crawl SUCCESS: {}'.format(item_info))
         self.chrome.quit()
         return item_info
 
@@ -97,8 +140,8 @@ if __name__ == '__main__':
     start = time.time()
     c = Crawler()
     # c = Crawler({'http': '125.105.32.168:7305', 'https': '171.211.32.79:2456'})
-    # logging.debug(c.get_jd_item('2777811'))
-    logging.debug(c.get_huihui_item('2777811'))
+    logging.debug(c.get_jd_item('2777811'))
+    # logging.debug(c.get_huihui_item('2777811'))
     # logging.debug(c.get_cate_item('https://list.jd.com/list.html?cat=9987,653,655&page=2'
     #                               '&sort=sort_rank_asc&trans=1&JL=6_0_0&ms=6#J_main'))
     end = time.time()
